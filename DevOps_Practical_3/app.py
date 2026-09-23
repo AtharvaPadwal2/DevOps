7596ec07a6eff320be36d26cd3ee882ec4b5bc2c
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-23 10:56:30 +0530

    update

--- DevOps_Practical_3/app.py
+++ DevOps_Practical_3/app.py
@@ -1,8 +1,8 @@
 print("Hello! Welcome to DevOps Practical 3")
 print("DevOps")
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int 20
+b = int 10
 
 print("Addition:", a + b)
 print("Subtraction:", a - b)
