a5c840818f5cb5aa83112f2e3cf51c229d668237
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-23 10:34:41 +0530

    Initial Commit

---
+++ DevOps_Practical_3/app.py
@@ -0,0 +1,8 @@
+print("Hello! Welcome to DevOps Practical 3")
+print("DevOps")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
